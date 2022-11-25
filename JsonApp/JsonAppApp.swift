b78fa667b2cccdc90d2b2eb7e485598f45a7274b
Luis Angel Torres G <t.g.luisangel@hotmail.com>
2022-11-24 23:20:59 -0600

    Initial Commit

---
+++ JsonApp/JsonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonAppApp.swift
+//  JsonApp
+//
+//  Created by Luis Angel Torres G on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct JsonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
